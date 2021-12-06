de8e450ee5ec47df9b5b03c3ac0231562eaca039
offsujin <offsujin@gmail.com>
2021-12-06 12:31:15 +0900

    change junit5 -> 4

--- src/test/java/jpabook/jpashop/JpashopApplicationTests.java
+++ src/test/java/jpabook/jpashop/JpashopApplicationTests.java
@@ -1,6 +1,6 @@
 package jpabook.jpashop;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -9,5 +9,4 @@ class JpashopApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
 }
