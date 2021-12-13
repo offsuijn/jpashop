cf0599ebb13e0527258e70ce1d7c64a3eb65e1d6
offsujin <offsujin@gmail.com>
2021-12-13 23:59:32 +0900

    commit

--- src/test/java/jpabook/jpashop/MemberRepositoryTest.java
+++ src/test/java/jpabook/jpashop/MemberRepositoryTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ public class MemberRepositoryTest {
 
     @Test
     @Transactional
+    @Rollback(False)
     public void testMember() throws Exception {
         //given
         Member member = new Member();
