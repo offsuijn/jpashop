cb11f10899a7087b454aa7e280d00bd4e4db7a68
offsujin <offsujin@gmail.com>
2021-12-25 22:58:51 +0900

    Implement Order Service

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.repository.ItemRepository;
+import jpabook.jpashop.repository.MemberRepository;
+import jpabook.jpashop.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class OrderService {
+
+    private final MemberRepository memberRepository;
+    private final OrderRepository orderRepository;
+    private final ItemRepository itemRepository;
+
+
+
+
+
+}
