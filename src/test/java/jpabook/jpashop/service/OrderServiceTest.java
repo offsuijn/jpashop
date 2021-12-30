5952cacc426237750a8c4fdd7777b6d6cf99d2d4
offsujin <offsujin@gmail.com>
2021-12-30 12:05:35 +0900

    Add Order Service Test

--- src/test/java/jpabook/jpashop/service/OrderServiceTest.java
+++ src/test/java/jpabook/jpashop/service/OrderServiceTest.java
@@ -53,7 +53,18 @@ public class OrderServiceTest {
 
     @Test(expected = NotEnoughStockException.class)
     public void 상품주문_재고수량초과() throws Exception {
-        //...
+
+        //Given
+        Member member = createMember();
+        Item item = createBook("JPA", 10000, 10); //이름, 가격, 재고
+
+        int orderCount = 11; //재고보다 많은 수량
+
+        //When
+        orderService.order(member.getId(), item.getId(), orderCount);
+
+        //Then
+        fail("재고 수량 부족 예외가 발생해야한다.");
     }
 
     @Test
